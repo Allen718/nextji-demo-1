a7f92a3d9ea92c3976b8de1485b14472de61b844
yyp <yuy20170301@126.com>
2022-03-15 10:57:16 +0800

    fix(lib/getDataBaseConnection): 修改生产环境数据库

--- lib/getDatabaseConnection.tsx
+++ lib/getDatabaseConnection.tsx
@@ -1,35 +1,36 @@
 
-import {createConnection, getConnectionManager} from "typeorm";
+import { createConnection, getConnectionManager } from "typeorm";
 import 'reflect-metadata';
-import {Post} from 'src/entity/Post';
-import {User} from 'src/entity/User';
-import {Comment} from 'src/entity/Comment';
+import { Post } from 'src/entity/Post';
+import { User } from 'src/entity/User';
+import { Comment } from 'src/entity/Comment';
 import config from 'ormconfig.json';
 
-const create=async ()=>{
-    // @ts-ignore
-    return createConnection({
-        ...config,
-        entities: [Post, User, Comment]
-    });
+const create = async () => {
+  // @ts-ignore
+  return createConnection({
+    ...config,
+    database: process.env.NODE_ENV === 'production' ? 'blog_production' : 'blog_development',
+    entities: [Post, User, Comment]
+  });
 }
 const promise = (async () => {
-    const manager = getConnectionManager()
-    const current = manager.has('default') && manager.get('default');
-    if (current) {await current.close();}
-    return create();
-    // if (!manager.has('default')) {
-    //    return create()
-    // } else {
-    //     const current = manager.get('default');
-    //     if (current.isConnected) {
-    //         return current
-    //     } else {
-    //         return await createConnection()
-    //     }
-    // }
+  const manager = getConnectionManager()
+  const current = manager.has('default') && manager.get('default');
+  if (current) { await current.close(); }
+  return create();
+  // if (!manager.has('default')) {
+  //    return create()
+  // } else {
+  //     const current = manager.get('default');
+  //     if (current.isConnected) {
+  //         return current
+  //     } else {
+  //         return await createConnection()
+  //     }
+  // }
 
 })();
 export const getDatabaseConnection = () => {
-    return promise
+  return promise
 }
