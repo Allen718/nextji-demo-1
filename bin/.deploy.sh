5119328ee2c2e4b8b0fd355b40d930a7c6ff3b5c
yyp <yuy20170301@126.com>
2022-03-25 18:38:02 +0800

    feat(自动化部署):部署脚本有问题

--- bin/.deploy.sh
+++ bin/.deploy.sh
@@ -2,13 +2,14 @@ docker start 05899 &&
 cd /home/blog/app/nextjs-demo-1&&
 git pull &&
 yarn install --production=false&&
-yarn m:run&&
-git apply entity.patch&&
+rm -rf dist&&
+yarn m:run &&
+git apply entity.patch &&
 yarn typeorm:build&&
-yarn build&&
-git reset --hard HEAD&&Ï
-docker kill app&&
-docker rm app&&
+yarn build &&
+git reset --hard HEAD&&
+docker kill app;
+docker rm app ;
 docker build . -t yang/node-web-app&&
 docker run --network=host --name app -p 3200:3200 -d yang/node-web-app&&
 echo 'OKK!'
