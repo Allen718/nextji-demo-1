9f38d9d011ddb7f49e88cb317255ad61f5f44819
yyp <yuy20170301@126.com>
2022-03-25 18:17:25 +0800

    feat(自动化部署): 添加自动化部署脚本

--- bin/.deploy.sh
+++ bin/.deploy.sh
@@ -1,12 +1,16 @@
-docker start 98a&&
+docker start 05899 &&
 cd /home/blog/app/nextjs-demo-1&&
-git pull&&
+git pull &&
 yarn install --production=false&&
+yarn m:run&&
+git apply entity.patch&&
+yarn typeorm:build&&
 yarn build&&
+git reset --hard HEAD&&Ï
 docker kill app&&
 docker rm app&&
 docker build . -t yang/node-web-app&&
 docker run --network=host --name app -p 3200:3200 -d yang/node-web-app&&
-echo 'ok!'
+echo 'OKK!'
 
 
